3cc49746579e8cd98cab810b4ad6495d9ca98ee3
Aiperi <aip.amankulova@gmail.com>
2020-07-12 20:34:21 +0600

    Binding TextView with its ID

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.homework
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,12 @@ class MainActivity : AppCompatActivity() {
         val secondETInArray = findViewById<EditText>(R.id.secondETInArray)
         val thirdETInArray =findViewById<EditText>(R.id.thirdETInArray)
         val fourthETInArray = findViewById<EditText>(R.id.fourthETInArray)
-        val fifthETInArray = findViewById<EditText>(R.id.fifthETInArray) 
+        val fifthETInArray = findViewById<EditText>(R.id.fifthETInArray)
 
+        val firstTVInArray =findViewById<TextView>(R.id.firstTVInArray)
+        val secondTVInArray = findViewById<TextView>(R.id.secondTVInArray)
+        val thirdTVInArray = findViewById<TextView>(R.id.thirdTVInArray)
+        val fourthTVInArray = findViewById<TextView>(R.id.fourthTVInArray)
+        val fifthTVInArray = findViewById<TextView>(R.id.fifthTVInArray)
     }
 }
