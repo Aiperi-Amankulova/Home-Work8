4d94567bbe49edf83c1ee0103341137da1a1dbd3
Aiperi <aip.amankulova@gmail.com>
2020-07-12 21:12:35 +0600

    We fix the array

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -35,7 +35,32 @@ class MainActivity : AppCompatActivity() {
         fourthETInArray.setText(array[3].toString())
         fifthETInArray.setText(array[4].toString())
 
-         
+        val a1 = -4
+        val b1 = 3.14
+        val c1 = a1 * b1
+        firstTVInArray.text = c1.toString()
+
+        val a2 = 45
+        val b2 = 3.14
+        val c2 = a2 * b2
+        secondTVInArray.text = c2.toString()
+
+        val a3 = 234
+        val b3 = 3.14
+        val c3 = a3 * b3
+        thirdTVInArray.text = c3.toString()
+
+        val a4 = 234
+        val b4 = 3.14
+        val c4 = a4 * b4
+        fourthTVInArray.text = c4.toString()
+
+        val a5 = 23
+        val b5 = 3.14
+        val c5 = a5 * b5
+        fifthTVInArray.text = c5.toString()
+
+
         }
     }
 }
