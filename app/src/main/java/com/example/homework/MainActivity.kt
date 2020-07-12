42b09d42013181618e54b3f8194ac795024c3e77
Aiperi <aip.amankulova@gmail.com>
2020-07-12 21:11:39 +0600

    Add an array to activity

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.homework
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,18 +12,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val array = arrayOf(4,45,234,234,23)
+
         val firstETInArray = findViewById<EditText>(R.id.firstETInArray)
         val secondETInArray = findViewById<EditText>(R.id.secondETInArray)
-        val thirdETInArray =findViewById<EditText>(R.id.thirdETInArray)
+        val thirdETInArray = findViewById<EditText>(R.id.thirdETInArray)
         val fourthETInArray = findViewById<EditText>(R.id.fourthETInArray)
         val fifthETInArray = findViewById<EditText>(R.id.fifthETInArray)
 
-        val firstTVInArray =findViewById<TextView>(R.id.firstTVInArray)
+        val firstTVInArray = findViewById<TextView>(R.id.firstTVInArray)
         val secondTVInArray = findViewById<TextView>(R.id.secondTVInArray)
         val thirdTVInArray = findViewById<TextView>(R.id.thirdTVInArray)
         val fourthTVInArray = findViewById<TextView>(R.id.fourthTVInArray)
         val fifthTVInArray = findViewById<TextView>(R.id.fifthTVInArray)
 
         val btnInArray = findViewById<Button>(R.id.btnInArray)
+
+
+        firstETInArray.setText(array[0].toString())
+        secondETInArray.setText(array[1].toString())
+        thirdETInArray.setText(array[2].toString())
+        fourthETInArray.setText(array[3].toString())
+        fifthETInArray.setText(array[4].toString())
+
+         
+        }
     }
 }
